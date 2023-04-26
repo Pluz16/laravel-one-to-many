30ea6ec4259af9f60eaef240e092b1283d0d1eb6
Francesco Punta <puntafrancesco@gmail.com>
2023-04-26 19:31:23 +0200

    Edit Request, CategoryPolicy

--- app/Http/Requests/Auth/UpdateCategoryRequest.php
+++ app/Http/Requests/Auth/UpdateCategoryRequest.php
@@ -3,9 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
-class UpdatePostRequest extends FormRequest
+class UpdateCategoryRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -14,7 +13,7 @@ class UpdatePostRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return false;
     }
 
     /**
@@ -25,13 +24,7 @@ class UpdatePostRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => [
-                'required',
-                'max:150',
-                Rule::unique('posts', 'title')->ignore($this->post)
-            ],
-            'content' => 'nullable|string',
-            'category_id' => 'nullable|exists:categories,id'
+            //
         ];
     }
 }
