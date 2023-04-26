ca1a8a4103f72f0dc46ede46d8215bb2e85e8c9d
Francesco Punta <puntafrancesco@gmail.com>
2023-04-26 19:56:11 +0200

    show category

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="d-flex align-items-center">
-            <div class="me-auto">
-                <h1>{{ $post->title }}</h1>
-                <p>/{{ $post->slug }}</p>
+        <div class="container py-5">
+        <div class="d-flex align-items-center justify-content-between">
+            <h1 class="me-auto">{{ $post->title }}</h1>
+            <div class="d-flex align-items-center">
+            @if($post->category)
+                <span class="badge rounded-pill bg-warning me-3">{{ $post->category->name }}</span>
+            @else
+                <span class="badge rounded-pill bg-secondary me-3">Nessuna categoria</span>
+            @endif
+            <a href="{{ route('posts.edit', $post) }}" class="btn btn-sm btn-primary">Modifica</a>
             </div>
+        </div>
+        </div>
+
 
             <div class="d-flex">
                 <a class="btn btn-sm btn-secondary" href="{{ route('posts.edit',$post) }}">Modifica</a>
